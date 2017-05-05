edabc75c6209f23e60663b8040b33ef21910368b
Alex <alexvillucci@gmail.com>
2017-05-05 14:27:56 -0700

    Context Files

--- MVCsln/ISINC_Project/DataAccess/IISINC_ProjectContext.cs
+++ MVCsln/ISINC_Project/DataAccess/IISINC_ProjectContext.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using ISINC_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using ISINC_Project.Models;
 
 namespace ISINC_Project.DataAccess
 {
     public interface IISINC_ProjectContext
     {
-        IQueryable<Product> GetAllProducts();
         IQueryable<Review> GetAllReviews();
-        IQueryable<Reviewer> GetAllReviewers();
+        IQueryable<Product> GetAllProducts();
+      
 
         void MarkAsModified(object obj);
         void MarkAsAdded(object obj);
