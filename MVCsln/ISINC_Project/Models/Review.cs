abc2ef83e878af17f118097475d08240e6a44f9c
Alex <alexvillucci@gmail.com>
2017-05-05 11:46:10 -0700

    made models for DB

---
+++ MVCsln/ISINC_Project/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace ISINC_Project.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public String Content { get; set; }
+        [Required]
+        public Product Product { get; set; }
+        public Reviewer Reviewer { get; set; }
+
+    }
+}
