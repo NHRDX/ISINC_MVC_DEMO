f4326e072eb039d65bac65e7fa2efc65de12f7c0
Alex <alexvillucci@gmail.com>
2017-05-05 15:09:43 -0700

    working on ienumerator

--- MVCsln/ISINC_Project/Controllers/ProductController.cs
+++ MVCsln/ISINC_Project/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ISINC_Project.Models;
+using System.Collections;
 
 namespace ISINC_Project.Controllers
 {
@@ -12,15 +13,21 @@ namespace ISINC_Project.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            //var IEnumerable<Product> = { }
+            //IEnumerable< Product pr> = null;
+
+            var prod1 = new { ID = 1, Name = "Airplane", Price = 49.99M, Description = "It just flies" };
+
+            var prod2 = new { ID = 2, Name = "Boat", Price = 39.99M, Description = "It just floats" };
+             
+            //IEnumerable{ prod1; prod2 };
 
 
+            // var IEnumerable<Product> = null;
+            //create a collection of products 
+            //... IEn
 
 
-           // var IEnumerable<Product> = null;
-           //create a collection of products 
-           //... IEn
-           //return it w the View method below
+            //return it w the View method below
             return View( );
         }
     }
