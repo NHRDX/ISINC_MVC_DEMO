779816eaa2e99419f97a571e067d6ba3692eeccc
Alex <alexvillucci@gmail.com>
2017-05-05 12:01:42 -0700

    Branch Created

---
+++ MVCsln/ISINC_Project/DataAccess/IISINC_ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using ISINC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISINC_Project.DataAccess
+{
+    public interface IProductDBContext
+    {
+        IQueryable<Product> GetAllProducts();
+        IQueryable<Review> GetAllReviews();
+        IQueryable<Reviewer> GetAllReviewers();
+
+        void MarkAsModified(object obj);
+        void MarkAsAdded(object obj);
+
+        void SaveChanges();
+    }
+}
